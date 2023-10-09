08eb9155228ef2bbd23448f08317353641e7a8fb
erbhai <bravelai635@gmail.com>
2023-10-09 07:34:57 +0545

    second try

---
+++ jpt.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+# Create a NumPy array
+np_array = np.array([1, 2, 3, 4, 5])
+
+# Create a Pandas DataFrame
+df = pd.DataFrame({'data': np_array})
+
+# Print the DataFrame
+print(df)
